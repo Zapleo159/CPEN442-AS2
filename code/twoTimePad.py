a01d7e1857e279f3f13dc78e4e7ac423af0981be
Ritam Singal <ritamsingal@gmail.com>
2023-11-03 18:33:06 -0700

    Substitution and twoTimePad working

--- code/twoTimePad.py
+++ code/twoTimePad.py
@@ -48,27 +48,17 @@ def testMessageText(file_name, test_word, ciphertexts_xor):
       test_word_binary_padded = left_pad + test_word_binary + right_pad
       xor_val = xor(ciphertexts_xor, test_word_binary_padded)
       relevant_xor = xor_val[i:i+len(test_word_binary)]
-      # print(f"{i}: {binary_to_string(relevant_xor)}")
       f.write(f"{i/7}: {binary_to_string(relevant_xor)}\n")
 
 def group6(file_name, test_word_formatted):
-  ciphertexts = readfile('./assignment2_files/q2_ciphertexts_group6.txt')
+  ciphertexts = readfile('./q2_ciphertexts_group6.txt')
   ciphertexts_xor = xor(ciphertexts[0], ciphertexts[1])
-  
-  test_word = 'Crop diversity loss threatens global food security, as the world\'s human population depends on a diminishing number of varieties of a diminishing number of crop species. Crops are increasingly grown in monoculture, meaning that if, as in the historic Great Famine of Ireland, a single disease overco'
-  # testMessageText(test_word, test_word, ciphertexts_xor)
-  testMessageText("full_text", test_word, ciphertexts_xor)
-  # testMessageText(file_name, test_word_formatted, ciphertexts_xor)
+  testMessageText(file_name, test_word_formatted, ciphertexts_xor)
 
 def group20():
-  plaintexts = readfile('./assignment2_files/q2_plaintexts_group20.txt')
-  print(plaintexts, '\n')
-  
-  ciphertexts = readfile('./assignment2_files/q2_ciphertexts_group20.txt')
-  # print(ciphertexts)
-  
-  keygroup = readfile('./assignment2_files/q2_key_group20.txt')
-  # print(keygroup)
+  plaintexts = readfile('../q2_plaintexts_group20.txt')
+  ciphertexts = readfile('../q2_ciphertexts_group20.txt')
+  keygroup = readfile('../q2_key_group20.txt')
   
   enc1 = encrypt(keygroup[0], plaintexts[0])
   enc2 = encrypt(keygroup[0], plaintexts[1])
@@ -81,12 +71,32 @@ def group20():
   print(dec1 == plaintexts[0])
   print(dec2 == plaintexts[1])
   
+def group6_find_key_and_plaintext_order():
+  ciphertexts = readfile('./assignment2_files/q2_ciphertexts_group6.txt')
+  
+  message = "Crop diversity loss threatens global food security, as the world's human population depends on a diminishing number of varieties of a diminishing number of crop species. Crops are increasingly grown in monoculture, meaning that if, as in the historic Great Famine of Ireland, a single disease overcom"
+  message_binary = string_to_binary(message)
+  secret_key = xor(message_binary, ciphertexts[0])
+  
+  plaintext0 = xor(ciphertexts[0], secret_key)
+  plaintext0 = binary_to_string(plaintext0)
+  plaintext1 = xor(ciphertexts[1], secret_key)
+  plaintext1 = binary_to_string(plaintext1)
+  
+  print(secret_key)
+  print(plaintext0)
+  print(plaintext1)
+  
   
 if __name__ == "__main__":
-  test_word = sys.argv[1]
   
-  if test_word != "":
-    test_words = test_word.split("_")
-    test_word_formatted = " ".join(test_words)
-    print(test_word_formatted)
-    group6(test_word, test_word_formatted)
+  if len(sys.argv) > 1:
+    test_word = sys.argv[1]    
+    if test_word != "":
+      test_words = test_word.split("_")
+      test_word_formatted = " ".join(test_words)
+      print(test_word_formatted)
+      group6(test_word, test_word_formatted)
+  
+  else:
+    group6_find_key_and_plaintext_order()
